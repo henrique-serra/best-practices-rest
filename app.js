7e5ca0498848371a0fb9515776d42df4fc3b9aba
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-10-01 18:14:11 +0000

    cache example

--- app.js
+++ app.js
@@ -1,47 +1,34 @@
 const express = require('express');
+const apicache = require('apicache');
 
 const app = express();
 
-const users = [{ email: 'abc@foo.com' }];
+let cache = apicache.middleware;
+app.use(cache('5 minutes'));
+
+const employees = [
+  { firstName: 'Jane', lastName: 'Smith', age: 20 },
+  { firstName: 'John', lastName: 'Smith', age: 30 },
+  { firstName: 'Mary', lastName: 'Green', age: 50 },
+];
 
 app.use(express.json());
 
-app.get('/articles', (req, res) => {
-  const articles = ['hello'];
-  res.json(articles);
-});
+app.get('/employees', (req, res) => {
+  // const { firstName, lastName, age } = req.query;
+  // let results = [...employees];
 
-app.post('/articles', (req, res) => {
-  res.json(req.body);
-});
+  // if (firstName) {
+  //   results = results.filter((r) => r.firstName === firstName);
+  // }
+  // if (lastName) {
+  //   results = results.filter((r) => r.lastName === lastName);
+  // }
+  // if (age) {
+  //   results = results.filter((r) => +r.age === +age);
+  // }
 
-app.put('/articles/:id', (req, res) => {
-  const { id } = req.params;
-
-  res.json(req.body);
-});
-
-app.delete('/articles/:id', (req, res) => {
-  const { id } = req.params;
-
-  res.json({ deleted: id });
-});
-
-app.get('/articles/:articleId/comments', (req, res) => {
-  const { articleId } = req.params;
-  const comments = [];
-
-  res.json(comments);
-});
-
-app.post('/users', (req, res) => {
-  const { email } = req.body;
-  const userExists = users.find((u) => u.email === email);
-  if (userExists) {
-    return res.status(400).json({ error: 'User already exists' });
-  }
-
-  res.json(req.body);
+  res.json(results);
 });
 
 app.listen(3000, () => {
