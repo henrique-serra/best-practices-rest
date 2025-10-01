ad37f904fe0d29c1dc74f80606a09ca8194985a4
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-10-01 21:13:31 +0000

    finish tutorial

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import models from './models/index.js';
+import routes from './routes/index.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}!`);
+});
